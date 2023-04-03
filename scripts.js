f26addca94b4b67d3142b66dae315968c2620c4e
Will.Co <120841470+willdev-crypto@users.noreply.github.com>
2023-04-03 17:26:30 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,43 @@
+        const objs = [
+        {
+            "nome":"William",
+            "idade": 30,
+            "esta trabahando": true,
+            "detalhes_profissao": {
+                "profissao": "Programador",
+                "empresa x":   
+            },
+            "hobbies": ["Programar,Ler,Musica"]
+            
+        },
+        
+        {
+            "nome":"joao",
+            "idade": 29,
+            "esta trabahando": false,
+            "detalhes_profissao": null, 
+                "profissao": "null",
+                "empresa": null,
+            },
+            "hobbies"["FUtebol,videogame"],
+            
+         {
+            
+        } while (condition);
+        do {
+            
+        } while (condition);
+        
+        console.log(objs)
+        
+        //JSON
+        // converter objeto para json
+        const jsonData=JSON.stringify(objs)
+
+        console.log(jsonData)
+        console.log(typeof jsonData)
+
+        //conervter json para objeto
+        const objData= JSON.parse(jsonData)
+ 
+        ]
